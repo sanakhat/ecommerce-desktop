1734f4cfc0737a9deaca7ceb19020af09c908030
sanakhat <121640228+sanakhat@users.noreply.github.com>
2023-12-31 03:06:09 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,56 @@
+﻿use [C:\USERS\ADMIN\ONEDRIVE\DESKTOP\DBMS\DBMS\SHOPPING.MDF];
+create table register(
+   username varchar(max),
+   Fname text not null,
+   Lname text not null,
+   email varchar(max) not null,
+   password varchar(max) not null
+)
+
+create table food(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
+
+create table cosmatics(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
+
+create table Electronics(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
+
+create table stationary(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
+
+create table clothes(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
+
+create table house_hold(
+	pro_id int IDENTITY(1,1) PRIMARY KEY,
+	price float,
+	pro_name varchar(max),
+	dis float,
+	stock int,
+)
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Signup]
+(
+	[User_name] NVARCHAR(MAX) NOT NULL PRIMARY KEY, 
+    [Email] NVARCHAR(MAX) unique NOT NULL, 
+    [First_name] TEXT NOT NULL, 
+    [Last_name] TEXT NOT NULL, 
+    [password] NVARCHAR(MAX) NOT NULL
+)
